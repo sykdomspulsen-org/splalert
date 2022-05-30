7e705b0e8b66a8294103f029c6e71e92cd622474
Chi Zhang <Chi.Zhang@fhi.no>
2022-05-30 14:16:05 +0200

    develop the example for msis cases

--- dev/x_test_short_term_trend.R
+++ dev/x_test_short_term_trend.R
@@ -149,4 +149,128 @@ q
 
 
 
+# ________ ----
+# msis cases examples ----
+
+
+d_msis <- spltidy::covid19_msis_cases_by_time_location
+colnames(d_msis)
+
+# show geogranularity and location code
+d_msis[, .(granularity_geo, location_code)] %>% unique
+
+# granularity time is week, day
+d_msis[, .(granularity_time)] %>% unique
+
+
+# select one location
+# d_msis[location_code == 'norge']
+d_msis[granularity_time == 'isoweek' & location_code == 'norge'] # 116 weeks
+
+
+d_fylke <- copy(d_msis[granularity_time == 'isoweek'& granularity_geo == 'county'] )
+
+d_fylke
+
+
+
+# fig 1 (n) ----
+# 1. fylke msis n
+q <- ggplot(d_fylke, aes(x = isoyearweek, y = covid19_cases_testdate_n))
+q <- q + geom_line(aes(group = location_code, col = location_code),size = 2)
+q <- q + scale_y_continuous("Weekly MSIS cases",
+                            expand = expansion(mult = c(0, 0.1)),
+                            labels = splstyle::format_nor_num_0)
+
+q <- q + scale_x_discrete("Isoyearweek",
+                          breaks = splstyle::every_nth(8))
+
+q <- q + splstyle::scale_color_fhi("Location", palette = "primary")
+q <- q + splstyle::theme_fhi_lines_horizontal(legend_position = "bottom")
+q <- q + splstyle::set_x_axis_vertical()
+# need watermark!!
+q
+
+
+
+# fig 2 (pr100000) ----
+# 2. fylke msis percentage
+q <- ggplot(d_fylke, aes(x = isoyearweek, y = covid19_cases_testdate_pr100000))
+q <- q + geom_line(aes(group = location_code, col = location_code),size = 2)
+q <- q + scale_y_continuous("Weekly MSIS cases (per 100 000)",
+                            expand = expansion(mult = c(0, 0.1)),
+                            labels = splstyle::format_nor_num_0)
+
+q <- q + scale_x_discrete("Isoyearweek",
+                          breaks = splstyle::every_nth(8))
+
+q <- q + splstyle::scale_color_fhi("Location", palette = "primary")
+q <- q + splstyle::theme_fhi_lines_horizontal(legend_position = "bottom")
+q <- q + splstyle::set_x_axis_vertical()
+# need watermark!!
+q
+
+
+# fig 3 (msis trend one loc) ----
+# msis trend
+
+# take a shorter time period, compute trend
+
+d_msis_norge <- copy(d_msis[granularity_time == 'isoweek' &
+                         isoyearweek >= '2021-26' &
+                         location_code == 'norge'])
+
+
+d_msis_norge
+
+
+trend_msis_norge <- splalert::short_term_trend(
+  d_msis_norge,
+  numerator = "covid19_cases_testdate_n",
+  trend_isoweeks = 6,
+  remove_last_isoweeks = 1
+)
+
+
+colnames(trend_msis_norge)
+
+# attach status for vis
+trend_msis_norge[, covid19_cases_testdate_trend0_42_status := factor(
+  covid19_cases_testdate_trend0_42_status,
+  levels = c("training","forecast","decreasing", "null", "increasing"),
+  labels = c("Training","Forecast","Decreasing", "Null", "Increasing")
+)]
+
+
+# plot
+q <- ggplot(trend_msis_norge, aes(x = isoyearweek,
+                            y = covid19_cases_testdate_forecasted_n,
+                            group = 1))
+q <- q + geom_col(mapping = aes(fill = covid19_cases_testdate_trend0_42_status ))
+q <- q + geom_errorbar(
+  mapping = aes(
+    ymin = covid19_cases_testdate_forecasted_predinterval_q02x5_n,
+    ymax = covid19_cases_testdate_forecasted_predinterval_q97x5_n
+  )
+)
+q <- q + scale_y_continuous("Weekly MSIS cases",
+                            expand = c(0, 0.1),
+                            labels = splstyle::format_nor_num_0)
+q <- q + scale_x_discrete("Isoyearweek", breaks = splstyle::every_nth(6))
+q <- q + expand_limits(y=0)
+q <- q + splstyle::scale_fill_fhi("6 week trend", palette = "contrast")
+q <- q + splstyle::theme_fhi_lines_horizontal(legend_position = "bottom")
+q <- q + splstyle::set_x_axis_vertical()
+q
+
+
+
+
+
+
+
+
+
+
+
 

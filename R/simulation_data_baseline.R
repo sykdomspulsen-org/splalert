20f4a77e8ead335419880eacb7f3012de5ebf2e1
Beatriz.ValcarcelSalamanca@fhi.no <Beatriz.ValcarcelSalamanca@fhi.no>
2022-05-30 15:04:16 +0200

    fix function

--- R/simulation_data_baseline.R
+++ R/simulation_data_baseline.R
@@ -38,46 +38,54 @@ periodic_pattern <- function(n_p=2,
         return(c)
 }
 
-#' simulate baseline data ----
-#' Simulate (daily) baseline data
+#' Simulate baseline data ----
+#' Simulation of baseline data.
 #'
 #' @description
-#' Simulates a time series of counts based on the Poisson/Negative Binomial model as described in Noufaily et al. (2019)
+#' Simulates a time series of counts in the absence of outbreaks. Simulation is based on a Poisson/Negative Binomial model as described in Noufaily et al. (2019).
+#' The funtion allowes to simulate properties of different syndromes/diasess, such as number of caess, tren, seaonality and weekly pattern.
 #'
 #' @param start_date Starting date of the simulation period.
 #'   Date is in the format of 'yyyy-mm-dd'.
 #'
 #' @param end_date Ending date of the simulation period.
+#'   Date is in the format of 'yyyy-mm-dd'.
 #'
-#' @param seasonal_pattern
+#' @param seasonal_pattern_n Number of seasonal patterns. For no seasonal pattern seasonal_pattern_n = 0. Seasonal_pattern_n = 1 represents annual pattern and seasonal_pattern_n = 2 indicates biannual pattern.
 #'
-#' @param weekly_pattern
+#' @param weekly_pattern_n Number of weekly patterns. For no specific weekly pattern, weekly_pattern_n = 0. Weekly_pattern_n = 1 represents one weekly peak.
 #'
-#' @param param_list List of parameters to control the baseline simulation.
-#'
+#' @param alpha
+#' @param beta Linear trend
+#' @param gamma_1 @param gamma_2 Seasonal trend
+#' @param gamma_3 @param gamma_4 Day-of-the week trend
+#' @param phi Dispersion parameter. If phi =0, a Poisson model is used to simulate baseline data
+#' @param shift_1 Horizontal shift parameter to help control over week/month peaks.
 #' @details
 #'
 #' @return
-#' A splfmt_rts_data_v1, data.table containing
+#' A splfmt_rts_data_v1, data.table containing a time series of counts
 #'
 #' \describe{
-#'   \item{granularity_time}{Pseudo ID for death events.}
-#'   \item{granularity_geo}{Pseudo ID for death events.}
-#'   \item{wday}{Pseudo ID for death events.}
+#'   \item{wday}{day-of-the week}
+#'   \item{n}{cases}
 #' }
-#'
 #'
 #' @export
 #' @examples
-#' start_date <- '2018-01-01'
-#' end_date <- '2019-12-31'
-#'
 #' baseline  <- simulate_baseline_data(
 #' start_date = as.Date("2012-01-01")
 #' end_date = as.Date("2019-12-31")
 #' seasonal_pattern_n = 1
 #' weekly_pattern_n = 1
-#' alpha = 3, beta = 0,gamma_1 = 0.8, gamma_2 = 0.6, gamma_3 = 0.8, gamma_4 = 0.4, phi = 4,shift_1 = 29 )
+#' alpha = 3,
+#' beta = 0,
+#' gamma_1 = 0.8,
+#' gamma_2 = 0.6,
+#' gamma_3 = 0.8,
+#' gamma_4 = 0.4,
+#' phi = 4,
+#' shift_1 = 29 )
 #' )
 
 
@@ -148,40 +156,31 @@ simulate_baseline_data <-  function(start_date,
 
 
 
-#' Simulate seasonal outbreak data ----
-#' Simulate (daily) seasonal outbreak data
+#' Simulate seasonal outbreaks ----
+#' Simulation of seasonal outbreaks for syndromes that follows seasonal trends.
+#' Seasonal outbreaks are more variable in size and timing than regular seasonal patterns.
+#' The number of seasonal outbreaks occur in a year are defined by n_season_outbreak.
+#' Parameters week_season_start and week_season_end define the season window.
+#' The start of the season is drawn from this season weeks, with high probability of outbreak around the peak of the season (week_season_peak).
+#' The seasonal outbreak is simulated as described in Noufaily et al. (2019).
 #'
 #' @description
-#' @param splfmt_rds_data
-#' Simulated or real data
-#' @param start_date Starting date simulated outbreak.
-#'   Date is in the format of 'yyyy-mm-dd'.
-#' @param numoutbk Ending date of the simulation period.
+#' @param data
+#' A splfmt_rds data object
+#' @param week_season_start Starting season week number
+#' @param week_season_peak Peak of the season week number
+#' @param week_season_end Ending season week number
+#' @param n_season_outbreak Number of seasonal outbreaks to be simulated
+#' @param m times the stardand deviation of the baseline count of staring day of the outbreak
+
 #' @details
-#'
 #' @return
-#' A splfmt_rts_data_v1, data.table containing
-#'
-#' \describe{
-#'   \item{granularity_time}{Pseudo ID for death events.}
-#'   \item{granularity_geo}{Pseudo ID for death events.}
-#'   \item{wday}{Pseudo ID for death events.}
-#' }
+#' A splfmt_rts_data_v1, data.table
 #'
 #'
 #' @export
 #' @examples
-#' start_date <- '2018-01-01'
-#' end_date <- '2019-12-31'
-#'
-#' baseline  <- simulate_baseline_data(
-#'  start_date = as.Date("2012-01-01")
-#'  end_date = as.Date("2019-12-31")
-#'  seasonal_pattern_n = 1
-#'  weekly_pattern_n = 2
-#' baseline_param_list = list(alpha = 3, beta = 0, gamma1 = 0.8, gamma2 = 0.6, gamma3 = 0.8, gamma4 = 0.4, phi = 4, shift = 29)
-#' outbreak_param_list = list(m=2)
-#' )
+
 
 simulate_seasonal_outbreak_data <-  function(data,
                                              week_season_start = 40,
@@ -270,41 +269,23 @@ simulate_seasonal_outbreak_data <-  function(data,
 
 
 
-#' simulate spike outbreak data ----
-#' Simulate (daily) spike outbreak data
+#' Simulate spiked outbreaks ----
+#' The method for simulating spiked outbreak is similar to seasonal outbreaks simulation but they are shorter in duration and are added only the year of data (prediction data) as described in Noufaily et al. (2019).
+#' Spiked outbreaks can start at any week of year of data
 #'
 #' @description
-#' @param splfmt_rds_data
-#' Simulated or real data
-#' @param start_date Starting date simulated outbreak.
-#'   Date is in the format of 'yyyy-mm-dd'.
-#' @param numoutbk Ending date of the simulation period.
+#' @param data
+#' A splfmt_rds data object
+#' @param n_sp_outbreak Number of seasonal outbreaks to be simulated
+#' @param m times the stardand deviation of the baseline count of staring day of the outbreak
+#'
 #' @details
-#'
 #' @return
-#' A splfmt_rts_data_v1, data.table containing
-#'
-#' \describe{
-#'   \item{granularity_time}{Pseudo ID for death events.}
-#'   \item{granularity_geo}{Pseudo ID for death events.}
-#'   \item{wday}{Pseudo ID for death events.}
-#' }
-#'
+#' A splfmt_rts_data_v1, data.table
 #'
 #' @export
 #' @examples
-#' start_date <- '2018-01-01'
-#' end_date <- '2019-12-31'
-#'
-#' baseline  <- simulate_baseline_data(
-#'  start_date = as.Date("2012-01-01")
-#'  end_date = as.Date("2019-12-31")
-#'  seasonal_pattern_n = 1
-#'  weekly_pattern_n = 2
-#' baseline_param_list = list(alpha = 3, beta = 0, gamma1 = 0.8, gamma2 = 0.6, gamma3 = 0.8, gamma4 = 0.4, phi = 4, shift = 29)
-#' outbreak_param_list = list(m=2)
-#'
-#' # )
+
 
 simulate_spike_outbreak_data <-  function(data,
                                           n_sp_outbreak =1,
@@ -371,31 +352,22 @@ simulate_spike_outbreak_data <-  function(data,
 
 
 
-#' add holiday effect ----
-#' add holiday efect to baseline data
+#' Holiday effect ----
+#' The effect of public holiday on count data
 #'
 #' @description
-#' @param splfmt_rds_data
-#' Simulated or real data
-#' @param start_date Starting date simulated outbreak.
-#'   Date is in the format of 'yyyy-mm-dd'.
-#' @param numoutbk Ending date of the simulation period.
+#' @param data
+#' A splfmt_rds data object
+#' @param holiday_data
+#' @param holiday_effect Ending date of the simulation period.
 #' @details
 #'
 #' @return
 #' A splfmt_rts_data_v1, data.table containing
 #'
-#' \describe{
-#'   \item{granularity_time}{Pseudo ID for death events.}
-#'   \item{granularity_geo}{Pseudo ID for death events.}
-#'   \item{wday}{Pseudo ID for death events.}
-#' }
-#'
-#'
 #' @export
 #' @examples
-
-# holiday_data <- fhidata::norway_dates_holidays
+#' holiday_data <- fhidata::norway_dates_holidays
 
 
 add_holiday_effect <-  function(data,
@@ -410,7 +382,6 @@ add_holiday_effect <-  function(data,
     holiday := is_holiday
   ]
 
-  d[, n:= n]
   d[holiday==T, n:= n*holiday_effect]
 
 
